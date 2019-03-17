536dbe4e8d651f6543c1904ed53ff209e272551c
Chris Pickel <sfiera@twotaled.com>
2019-03-17 10:23:46 +0900

    Define _USE_MATH_DEFINES for M_PI
    Necessary on Windows

--- src/cpp/test/float.test.cpp
+++ src/cpp/test/float.test.cpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#define _USE_MATH_DEFINES
+
 #include <gmock/gmock.h>
 #include <pn/procyon.h>
 #include <cmath>
