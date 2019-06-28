2b32f25086ed8c1405cb891e17f6bc62239f9c67
Chris Pickel <sfiera@twotaled.com>
2019-06-27 21:31:23 -0400

    Store more unicode data

--- src/c/scripts/unicode.py
+++ src/c/scripts/unicode.py
@@ -26,13 +26,56 @@ UNICODE_DATA_URL = "http://www.unicode.org/Public/UCD/latest/ucd/UnicodeData.txt
 EAST_ASIAN_WIDTH_FILE = "/tmp/EastAsianWidth.txt"
 EAST_ASIAN_WIDTH_URL = "http://www.unicode.org/Public/UCD/latest/ucd/EastAsianWidth.txt"
 
+CATEGORY = {
+    "Cc": 0o00,
+    "Cf": 0o01,
+    "Cn": 0o02,
+    "Co": 0o03,
+    "Cs": 0o04,
+    "Ll": 0o10,
+    "Lm": 0o11,
+    "Lo": 0o12,
+    "Lt": 0o13,
+    "Lu": 0o14,
+    "Mc": 0o20,
+    "Me": 0o21,
+    "Mn": 0o22,
+    "Nd": 0o30,
+    "Nl": 0o31,
+    "No": 0o32,
+    "Pc": 0o40,
+    "Pd": 0o41,
+    "Pe": 0o42,
+    "Pf": 0o43,
+    "Pi": 0o44,
+    "Po": 0o45,
+    "Ps": 0o46,
+    "Sc": 0o50,
+    "Sk": 0o51,
+    "Sm": 0o52,
+    "So": 0o53,
+    "Zl": 0o60,
+    "Zp": 0o61,
+    "Zs": 0o62,
+}
+
+EAST_ASIAN_WIDTH = {
+    "A": 0o000,
+    "F": 0o100,
+    "H": 0o000,
+    "N": 0o000,
+    "Na": 0o000,
+    "W": 0o100,
+}
+
 
 def main():
     unidata = cache_unidata()
     eawdata = cache_east_asian_widths()
-    widths = [-1 for i in range(0x110000)]
+    default = CATEGORY["Cn"] | EAST_ASIAN_WIDTH["F"]
+    data = [default for i in range(0x110000)]
 
-    # First, set width to 2 for anything with East Asian Width "W" (wide) or "F" (full-width).
+    # First, clear double-width bit for anything East Asian Width A/H/N/Na.
     for line in eawdata.splitlines():
         if "#" in line:
             line, _ = line.split("#", 1)
@@ -40,9 +83,7 @@ def main():
         if not line:
             continue
 
-        cprange, eaw_class = line.split(";")
-        if eaw_class not in "WF":
-            continue
+        cprange, eaw = line.split(";")
         if ".." in cprange:
             cprange = tuple(int(cp, 16) for cp in cprange.split(".."))
         else:
@@ -50,27 +91,28 @@ def main():
             cprange = (cp, cp)
 
         for cp in range(cprange[0], cprange[1] + 1):
-            widths[cp] = 2
+            data[cp] = (data[cp] & 0o077) | EAST_ASIAN_WIDTH[eaw]
 
-    # Second, set width to 0 for all modifiers, and clear width for control/space characters.
-    # Overrides the above, e.g. in U+3099 COMBINING KATAKANA-HIRAGANA VOICED SOUND MARKER, which is
-    # a wide modifier and has width 0 like any other modifier.
+    # Second, set category bits.
+    firsts = {}
+    lasts = {}
     for line in unidata.splitlines():
-        cp, _, class_, _ = line.split(";", 3)
+        cp, name, category, _ = line.split(";", 3)
         cp = int(cp, 16)
-
-        if class_.startswith("M"):
-            widths[cp] = 0
-        elif class_.startswith("C") or class_.startswith("Z"):
-            widths[cp] = -1
+        if name.endswith(", First>"):
+            firsts[name[:-8]] = (cp, category)
+        elif name.endswith(", Last>"):
+            lasts[name[:-7]] = cp
         else:
-            if widths[cp] == -1:
-                widths[cp] = 1
+            data[cp] = (data[cp] & 0o100) | CATEGORY[category]
 
-    widths[ord(" ")] = 1
-    widths[ord("　")] = 2
+    assert frozenset(firsts) == frozenset(lasts)
+    for name, (first, category) in firsts.items():
+        last = lasts[name]
+        for cp in range(first, last + 1):
+            data[cp] = (data[cp] & 0o100) | CATEGORY[category]
 
-    size = print_level("widths", widths)
+    size = print_level("data", data)
     print("// %d bytes" % size)
 
 
@@ -108,8 +150,8 @@ def print_level(name, values, level=0):
         return (csize * block_size * len(blocks)) + print_level(
             name, [blocks.get(x) for x in next_values], level + 1)
     else:
-        print("static const %s %s_%d[] = {%s};" % (ctype, name, level,
-                                                   ", ".join(map(str, values))))
+        print("static const %s %s_%d[] = {%s};" %
+              (ctype, name, level, ", ".join(map(str, values))))
         return csize * len(values)
 
 
